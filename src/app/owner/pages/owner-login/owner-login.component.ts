0394c5d520046b631883625e47aa4d761628681d
Mohith K <mohith81999@gmail.com>
2025-12-10 16:07:52 -0800

    Add optional country field and password rules display to login page

--- src/app/owner/pages/owner-login/owner-login.component.ts
+++ src/app/owner/pages/owner-login/owner-login.component.ts
@@ -1,7 +1,8 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { OwnerAuthService } from '../../../core/services/owner-auth.service';
+import { CityService, Country } from '../../../core/services/city.service';
 
 @Component({
   selector: 'app-owner-login',
@@ -9,17 +10,44 @@ import { OwnerAuthService } from '../../../core/services/owner-auth.service';
   styleUrls: ['./owner-login.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class OwnerLoginComponent {
+export class OwnerLoginComponent implements OnInit {
+  countries: Country[] = [];
+  showPassword = false;
+  showPasswordRules = false;
+
   loginForm = this.fb.group({
     email: ['', [Validators.required, Validators.email]],
-    password: ['', Validators.required]
+    password: ['', Validators.required],
+    countryId: ['']
   });
 
   constructor(
     private readonly fb: FormBuilder,
     private readonly ownerAuth: OwnerAuthService,
-    private readonly router: Router
+    private readonly router: Router,
+    private readonly cityService: CityService
   ) {}
+
+  ngOnInit(): void {
+    this.loadCountries();
+  }
+
+  loadCountries(): void {
+    this.cityService.getCountries().subscribe((countries) => {
+      this.countries = countries;
+    });
+  }
+
+  togglePasswordVisibility(): void {
+    this.showPassword = !this.showPassword;
+  }
+
+  onPasswordBlur(): void {
+    // Hide password rules when field loses focus
+    setTimeout(() => {
+      this.showPasswordRules = false;
+    }, 300);
+  }
 
   submit() {
     if (this.loginForm.invalid) {
