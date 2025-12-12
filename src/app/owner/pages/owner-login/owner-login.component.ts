da7fcb9d40e2e85d28f4d2e1d5d310860a1e4595
Mohith K <mohith81999@gmail.com>
2025-12-12 11:06:26 -0800

    Clean up login UI - remove country field and password rules box

--- src/app/owner/pages/owner-login/owner-login.component.ts
+++ src/app/owner/pages/owner-login/owner-login.component.ts
@@ -1,8 +1,7 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { OwnerAuthService } from '../../../core/services/owner-auth.service';
-import { CityService, Country } from '../../../core/services/city.service';
 
 @Component({
   selector: 'app-owner-login',
@@ -10,43 +9,22 @@ import { CityService, Country } from '../../../core/services/city.service';
   styleUrls: ['./owner-login.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class OwnerLoginComponent implements OnInit {
-  countries: Country[] = [];
+export class OwnerLoginComponent {
   showPassword = false;
-  showPasswordRules = false;
 
   loginForm = this.fb.group({
     email: ['', [Validators.required, Validators.email]],
-    password: ['', Validators.required],
-    countryId: ['']
+    password: ['', Validators.required]
   });
 
   constructor(
     private readonly fb: FormBuilder,
     private readonly ownerAuth: OwnerAuthService,
-    private readonly router: Router,
-    private readonly cityService: CityService
+    private readonly router: Router
   ) {}
-
-  ngOnInit(): void {
-    this.loadCountries();
-  }
-
-  loadCountries(): void {
-    this.cityService.getCountries().subscribe((countries) => {
-      this.countries = countries;
-    });
-  }
 
   togglePasswordVisibility(): void {
     this.showPassword = !this.showPassword;
-  }
-
-  onPasswordBlur(): void {
-    // Hide password rules when field loses focus
-    setTimeout(() => {
-      this.showPasswordRules = false;
-    }, 300);
   }
 
   submit() {
